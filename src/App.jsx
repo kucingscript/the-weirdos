98cb14716e1dce42ee04d275ac91fd5ec5782c72
rasyidzkun <rasyidzkun@gmail.com>
2022-09-17 20:16:29 +0700

    react lazy

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,27 @@
-import {
-  Navbar,
-  Home,
-  About,
-  Team,
-  Showcase,
-  Faq,
-  Footer,
-  ScrollToTop,
-} from "./components";
+import { lazy, Suspense } from "react";
+import { Navbar, Home, ScrollToTop } from "./components";
+
+const Team = lazy(() => import("./components/Team/Team"));
+const About = lazy(() => import("./components/About/About"));
+const Showcase = lazy(() => import("./components/Showcase/Showcase"));
+const Faq = lazy(() => import("./components/Faq/Faq"));
+const Footer = lazy(() => import("./components/Footer/Footer"));
+
+const fallback = () => <p>Loading....</p>;
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Home />
-      <About />
-      <Team />
-      <Showcase />
-      <Faq />
-      <Footer />
       <ScrollToTop />
+      <Suspense fallback={fallback()}>
+        <About />
+        <Team />
+        <Showcase />
+        <Faq />
+        <Footer />
+      </Suspense>
     </>
   );
 };
